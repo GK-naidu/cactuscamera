7d30e72a42f043653de5ec58e6b4945098cb07e3
G.K.Naidu <plurals.reboots-0y@icloud.com>
2025-11-01 14:39:09 +0530

    Initial Commit

---
+++ cactus camera/cactus_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  cactus_cameraApp.swift
+//  cactus camera
+//
+//  Created by GK Naidu on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct cactus_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
